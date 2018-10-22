de11491c3ec11b4a3f9014a4add5bca3467fa29a
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-23 01:21:26 +0200

    add more control statements to insure that the last image alt is printed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 document.querySelector(".grid").addEventListener("click", function(e) {
-    var removeTarget = e.target.parentNode;
-    removeTarget.parentNode.removeChild(removeTarget);
+    if (e.target.tagName === "IMG") { // Check if the images only is clicked
+        var liLength = this.querySelectorAll("li").length; // Store the length of li
+        if (liLength > 1) { // Check if the li more than one item
+            var removeTarget = e.target.parentNode; // Store the parent node to be removed
+            removeTarget.parentNode.removeChild(removeTarget); // remove the parent node of the last variable
+        } else { // grab the last image
+            var imgTitle = e.target.alt; // Store the last image alt value
+            document.querySelector("#art p").innerHTML = "<p>You picked: " + imgTitle + "</p>"; // print the alt value in the p element
+        }
+    }
 }, false);
